e5e466bdc603b87fb9a6f7165072ef869c23a141
themepro <35577973+themepro@users.noreply.github.com>
2019-09-13 12:50:55 +0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "demo@gmail.com";
+        $mail_to = "tuhin0680@yahoo.com";
         
         # Sender Data
         $subject = trim($_POST["subject"]);
